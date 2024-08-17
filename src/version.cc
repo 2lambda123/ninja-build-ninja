f58ee7802a427e9f845473ea4fd88885e96e9230
Restyled.io <commits@restyled.io>
2024-08-17 13:31:09 +0000

    Restyled by clang-format

--- src/version.cc
+++ src/version.cc
@@ -23,33 +23,35 @@ using namespace std;
 const char* kNinjaVersion = "1.12.1";
 
 void ParseVersion(const string& version, int* major, int* minor) {
-    size_t end = version.find('.');
-    *major = atoi(version.substr(0, end).c_str());
-    *minor = 0;
-    if (end != string::npos) {
-        size_t start = end + 1;
-        end = version.find('.', start);
-        *minor = atoi(version.substr(start, end).c_str());
-    }
+  size_t end = version.find('.');
+  *major = atoi(version.substr(0, end).c_str());
+  *minor = 0;
+  if (end != string::npos) {
+    size_t start = end + 1;
+    end = version.find('.', start);
+    *minor = atoi(version.substr(start, end).c_str());
+  }
 }
 
 void CheckNinjaVersion(const string& version) {
-    int bin_major, bin_minor;
-    ParseVersion(kNinjaVersion, &bin_major, &bin_minor);
-    int file_major, file_minor;
-    ParseVersion(version, &file_major, &file_minor);
+  int bin_major, bin_minor;
+  ParseVersion(kNinjaVersion, &bin_major, &bin_minor);
+  int file_major, file_minor;
+  ParseVersion(version, &file_major, &file_minor);
 
-    if (bin_major > file_major) {
-        Warning("ninja executable version (%s) greater than build file "
-                "ninja_required_version (%s); versions may be incompatible.",
-                kNinjaVersion, version.c_str());
-        return;
-    }
+  if (bin_major > file_major) {
+    Warning(
+        "ninja executable version (%s) greater than build file "
+        "ninja_required_version (%s); versions may be incompatible.",
+        kNinjaVersion, version.c_str());
+    return;
+  }
 
-    if ((bin_major == file_major && bin_minor < file_minor) ||
-            bin_major < file_major) {
-        Fatal("ninja version (%s) incompatible with build file "
-              "ninja_required_version version (%s).",
-              kNinjaVersion, version.c_str());
-    }
+  if ((bin_major == file_major && bin_minor < file_minor) ||
+      bin_major < file_major) {
+    Fatal(
+        "ninja version (%s) incompatible with build file "
+        "ninja_required_version version (%s).",
+        kNinjaVersion, version.c_str());
+  }
 }
